ff086d030efd5c57c34352d83e7e9eebed64edd6
Guilherme Torres <guilhermetorres97@gmail.com>
2017-08-27 19:03:30 -0300

    test files

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,11 @@
 int main(int argc, char **argv)
 {
 	FILE *rom = fopen(argv[1], "rb");
-	char current_instruction;
+	unsigned char current_instruction;
 	while(!feof(rom))
 	{
 		fread(&current_instruction, sizeof(char), 1, rom);
-		printf("%x\n", current_instruction);
+		printf("%02x\n", current_instruction);
 	}
 
 	fclose(rom);
