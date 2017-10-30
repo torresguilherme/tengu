a1f0aa690a258cdf9598a1e538325dab9d9b2bca
Guilherme Torres <guilhermetorres97@gmail.com>
2017-10-30 18:16:04 -0200

    CPU initiated

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -2,4 +2,13 @@
 
 CPU::CPU()
 {
+	PC = 0;
+	stack_pointer = 0;
+	accumulator = 0;
+	index_registerX = 0;
+	index_registerY = 0;
+	for(int i = 0; i < 7; i++)
+	{
+		status_register[i] = false;
+	}
 }
--- src/cpu.h
+++ src/cpu.h
@@ -4,4 +4,26 @@ class CPU
 {
 	public:
 	CPU();
+
+	private:
+	// registers
+	unsigned short int PC;
+	unsigned int stack_pointer;
+	unsigned char accumulator;
+	unsigned char index_registerX;
+	unsigned char index_registerY;
+	
+	// status flags
+	enum flags
+	{
+		CARRY,
+		ZERO,
+		INTERRUPT_DISABLE,
+		DECIMAL_MODE,
+		BREAK_COMMAND,
+		OVERFLOW,
+		NEGATIVE
+	};
+
+	bool status_register[7];
 };
