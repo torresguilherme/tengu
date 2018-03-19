ea68446d1cbfc04250e35656b7d20d655261fc27
Guilherme Torres <guilhermetorres97@gmail.com>
2018-03-19 15:23:56 -0300

    CPU registers ready

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,14 +1,44 @@
-use std::{u8, u16, u32, i8};
+use std::{u8, u16};
+
+macro_rules! statusFlag
+{
+        /*
+                status register flag bits:
+                0: carry
+                1: zero
+                2: interrupt
+                3: decimal
+                4: software interrupt (BRK)
+                5: doesn't exist, is set to logical 1 at all times
+                6: overflow
+                7: negative
+        */
+        (type: u8) =>
+        {
+                status & (1 << type)
+        }
+}
+
+macro_rules! setFlag
+{
+        (type: u8) =>
+        {
+                status |= (1 << type)
+        }
+}
 
 // CPU Ricoh RP2A03, based on MOS6502
 pub struct CPU
 {
+        // program counter
         pub pc: u16,
-        pub a: u8,
-        pub b: u8,
+        // general purpose registers
+        pub a: u8, // accumulator
         pub x: u8,
         pub y: u8,
-        pub status: u8
+
+        pub sp: u8, //stack pointer
+        pub status: u8 // status register
 }
 
 impl CPU
@@ -18,11 +48,11 @@ impl CPU
                 (oldAddr & 0xff00) + newAddr
         }
         // stack functions
-        fn push(&self)
+        fn push(&mut self)
         {
         }
 
-        fn pop(&self)
+        fn pop(&mut self)
         {
         }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 extern crate sdl2;
-use std::io;
 mod cpu;
 
 fn main()
 {
-    let mut m_cpu = cpu::CPU {pc: 0xc000, a:0, b:0, x:0, y:0, status:0};
+    let mut m_cpu = cpu::CPU {pc: 0xc000, a:0, x:0, y:0, sp:0, status: 0x20};
     println!("{}", m_cpu.pc);
 }
